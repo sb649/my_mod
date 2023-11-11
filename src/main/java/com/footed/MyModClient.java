1043f3cc045684156473c1ad7bff66b0ffd7ad73
nq429 <nq429d6142@163.com>
2023-11-11 14:58:47 +0800

    Initial commit

---
+++ src/main/java/com/footed/MyModClient.java
@@ -0,0 +1,10 @@
+package com.footed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
